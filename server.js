f857bb988bfde6a95039532f9c8622d9bfd36c90
Laura Carlotta <carlotta.custodio@gmail.com>
2020-10-27 17:32:55 -0300

    heroku-teste2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get("/", (request, response) => {
     response.send('<span style="color: #F00;">Hello World!</span>');
 })
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
